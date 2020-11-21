790fcf9bb6fbfd9f62f343fc944103ccda9d0a86
rahilshah13 <rahil@vt.edu>
2020-11-20 21:13:01 -0500

    sockets working

---
+++ backend/app.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+/*CORS stands for Cross Origin Resource Sharing and allows modern web browsers to be able to send AJAX requests and receive HTTP responses for resource from other domains other that the domain serving the client side application.*/
+const cors = require('cors');
+
+//Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const bodyParser = require('body-parser');
+
+// Our JWT logic. Uses express-jwt which is a middleware that validates JsonWebTokens and sets req.user.
+const jwt = require('./_helpers/jwt');
+
+
+// Our error handler
+const errorHandler = require('./_helpers/error-handler');
+
+
+//socket.io
+const server = http.createServer(app);
+const io = require('socket.io')(server, {origins: '*:*'});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(jwt());
+
+app.use('/user', require('./routes/user.router'));
+// app.use('/course', require('./routes/course.router'));
+// app.use('/attendance', require('./routes/attendance.router'));
+//app.use(errorHandler);
+
+
+//SOCKET.IO
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('my message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('my broadcast', `server: ${msg}`);
+  });
+
+  socket.on('mouse_output', (data) => {
+    //console.log('Drawing Data Received: ' + data);
+    io.emit('mouse_input', data);
+  });
+
+});
+
+
+
+// start server
+//const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(4000, function () {
+  console.log('Server listening on port ' + 4000);
+});
+
+server.listen(5000, () => {
+  console.log('listening on *:5000');
+});
---
+++ backend/services/user.service.js
@@ -0,0 +1,84 @@
+const config = require('../config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('../_helpers/database');
+const User = db.User;
+
+module.exports = {
+    authenticate,
+    getAllUsers,
+    getById,
+    addUser,
+    //registerCourse
+}
+
+async function authenticate({ username, password }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+
+}
+
+async function getAllUsers() {
+    //Returning the result of the promise. In the next homework we will make sure no passwords are sent back to the user.
+    return User.find().select('-hash');
+}
+
+
+
+async function getById(id) {
+    return User.find({_id:id});
+}
+
+//TODO: finish this function. Here you should insert the course 'id' into the User document.
+// Useful Hint: Recall that JWT token already contains user id and with each client request the 'req' object is modified to include user id in req.user.sub
+// async function registerCourse(req){
+//
+//     let id = req.user.sub;
+//     const user = await User.findById(id);
+//
+//     if (user.role === 'Student') {
+//         //TODO: Do not allow students to register more than five course.
+//         if (user.courses.length < 5) {
+//             user.courses.push(req.body.courseid);
+//             await user.save();
+//         }
+//         else {
+//             throw "Course limit is reached. Remove a course to add a new one.";
+//         }
+//     }
+//         // //TODO: On the angular side you will need to hide the 'add' button from the professors, however, you should still block them from adding courses here as well.
+//     // //TODO: send a message to users if the limit is reached.
+//     else {
+//         throw "You must be a student to register a course.";
+//     }
+// }
+
+async function addUser(userParam) {
+
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+    else  if (await User.findOne({ email: userParam.email })) {
+        throw 'Email "' + userParam.email + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+
+}
+
